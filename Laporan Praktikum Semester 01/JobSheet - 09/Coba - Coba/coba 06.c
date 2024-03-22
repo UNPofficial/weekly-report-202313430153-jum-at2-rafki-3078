fd2112ddb14fb9aaf88ddf516be47c91026a8ad3
Muhammad Rafki <145805421+rafki-3078@users.noreply.github.com>
2024-03-22 22:14:28 +0700

    Add files via upload

---
+++ Laporan Praktikum Semester 01/JobSheet - 09/Coba - Coba/coba 06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//create by 23343078_muhammad rafki
+
+void add_score(int score) {
+	
+	score = score + 5;
+	printf("scire diubah ke %d\n", score);
+	
+}
+
+void main(){
+	
+	int score = 0;
+	
+	printf("score sebelum diubah : %d\n", score);
+	add_score(score);
+	printf("score setelah diubah : %d\n", score);
+	
+}
